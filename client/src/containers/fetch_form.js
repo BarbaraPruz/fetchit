fc71b948738e8df3c6d925a1e33a0641416e765f
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-11-08 15:25:45 -0800

    Add FetchForm to select http method and response code (no styling)

--- client/src/containers/fetch_form.js
+++ client/src/containers/fetch_form.js
@@ -3,10 +3,34 @@ import React, { Component } from 'react';
 
 class FetchForm extends Component {
 
+    state = {
+        method: 'get',
+        responseCode: '200',
+    }
+
+    handleChange = (event) => this.setState({[event.target.name]: event.target.value});
+
+    handleSubmit = (event) => {
+        event.preventDefault(); 
+        console.log("Submit for test",this.state)
+    }
+
     render() {            
         return (
             <React.Fragment>
                 <h1>Fetch Form</h1>
+                <form onSubmit={ event => this.handleSubmit(event) }>
+                    <select name="method" value={this.state.method} onChange={ event => this.handleChange(event)}>
+                        <option value="get">Get</option>
+                        <option value="post">Post</option>                    
+                    </select> 
+                    <select name="responseCode" value={this.state.responseCode} onChange={ event => this.handleChange(event)}>
+                        <option value="200">200</option>
+                        <option value="400">400</option>
+                        <option value="500">500</option>                                            
+                    </select>                                      
+                    <button type="submit" >Test</button>
+                </form>                
             </React.Fragment> 
         )
     }
